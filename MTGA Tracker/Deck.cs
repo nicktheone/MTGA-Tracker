24f90a683aa81dfaf9f03b9bf6714354f2583c66
Nicholas Martini <nicho.martini@gmail.com>
2019-01-31 18:22:12 +0100

    Removed unnecessary Using

--- MTGA Tracker/Deck.cs
+++ MTGA Tracker/Deck.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace MTGA_Tracker
